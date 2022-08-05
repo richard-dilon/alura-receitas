59e99596b6189a6777b0814ed93f298bbabc70cd
Richard Dilon <90274766+richard-dilon@users.noreply.github.com>
2022-08-05 20:04:18 -0300

    Updating repository

--- alurareceitas/alurareceitas/settings.py
+++ alurareceitas/alurareceitas/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'alura_receita',
         'USER': 'postgres',
-        'PASSWORD': 'redmist',
+        'PASSWORD': 'Aa.12345',
         'HOST': 'localhost'
     }
 }
