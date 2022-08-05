a03c9426f6b58764f9469b0c7f5456a377745270
Richard Dilon <90274766+richard-dilon@users.noreply.github.com>
2022-08-05 19:36:18 -0300

    Creating repository

---
+++ alurareceitas/receitas/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import get_object_or_404, render
+from .models import Receita
+
+def index (request):
+
+    receitas = Receita.objects.order_by('-data_receita').filter(publicada=True)
+
+    dados = {
+        'receitas' : receitas
+    }
+
+    return render(request, 'index.html', dados)
+
+def receita (request, receita_id):
+    receita = get_object_or_404(Receita, pk=receita_id)
+
+    receita_a_exibir = {
+        'receita' : receita
+    }
+
+    return render(request, 'receita.html', receita_a_exibir)
+
+def buscar(request):
+    lista_receitas = Receita.objects.order_by('-data_receita').filter(publicada=True)
+
+    if 'search' in request.GET:
+        nome_a_buscar = request.GET['search']
+        if nome_a_buscar:
+            lista_receitas = lista_receitas.filter(nome_receita__icontains=nome_a_buscar)
+
+    dados = {
+        'receitas' : lista_receitas
+    }
+
+    return render(request, 'buscar.html', dados)
