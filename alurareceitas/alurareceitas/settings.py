ea639d4935701422927fff897b717cbdfa473243
Richard Dilon <90274766+richard-dilon@users.noreply.github.com>
2022-08-08 21:28:15 -0300

    Criando a aplicação de cadastro/login de usuários

--- alurareceitas/alurareceitas/settings.py
+++ alurareceitas/alurareceitas/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'usuarios',
     'pessoas',
     'receitas',
     'django.contrib.admin',
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'alurareceitas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'receitas/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
