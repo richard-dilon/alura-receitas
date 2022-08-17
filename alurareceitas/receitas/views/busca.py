e4a774325a64adda4d16c5bb8d51cddf70900679
Richard Dilon <90274766+richard-dilon@users.noreply.github.com>
2022-08-17 13:02:45 -0300

    Modularização das views

---
+++ alurareceitas/receitas/views/busca.py
@@ -0,0 +1,17 @@
+from django.shortcuts import get_object_or_404, get_list_or_404, render, redirect
+from django.contrib import auth, messages
+from receitas.models import Receita
+
+def busca (request):
+    lista_receitas = Receita.objects.order_by('-data_receita').filter(publicada=True)
+
+    if 'search' in request.GET:
+        nome_a_buscar = request.GET['search']
+        if nome_a_buscar:
+            lista_receitas = lista_receitas.filter(nome_receita__icontains=nome_a_buscar)
+
+    dados = {
+        'receitas' : lista_receitas
+    }
+
+    return render(request, 'receitas/buscar.html', dados)
--- alurareceitas/receitas/views/receita.py
+++ alurareceitas/receitas/views/receita.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, get_list_or_404, render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth, messages
-from .models import Receita
+from receitas.models import Receita
 
 def index (request):
 
@@ -21,20 +21,6 @@ def receita (request, receita_id):
     }
 
     return render(request, 'receitas/receita.html', receita_a_exibir)
-
-def buscar(request):
-    lista_receitas = Receita.objects.order_by('-data_receita').filter(publicada=True)
-
-    if 'search' in request.GET:
-        nome_a_buscar = request.GET['search']
-        if nome_a_buscar:
-            lista_receitas = lista_receitas.filter(nome_receita__icontains=nome_a_buscar)
-
-    dados = {
-        'receitas' : lista_receitas
-    }
-
-    return render(request, 'receitas/buscar.html', dados)
 
 def cria_receita (request):
     if request.method == 'POST':
